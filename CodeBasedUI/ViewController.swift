b05d9416841fc264d9ce96f8eb3153c6681ef65b
星野恵瑠 <shonetesmith@gmail.com>
2015-01-04 21:57:25 +0900

    Initial Commit

---
+++ CodeBasedUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodeBasedUI
+//
+//  Created by 星野恵瑠 on 2015/01/04.
+//  Copyright (c) 2015年 星野恵瑠. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
