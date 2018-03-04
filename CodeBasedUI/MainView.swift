f4c05b46e541140abadaec40d0b46b94b1bf02ab
星野恵瑠 <shonetesmith@gmail.com>
2018-03-04 13:32:39 +0900

    Update source code according to current Swift 4 syntax, and fix some logic

--- CodeBasedUI/MainView.swift
+++ CodeBasedUI/MainView.swift
@@ -9,56 +9,51 @@
 import UIKit
 
 class MainView: UIView {
-
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
-    }
-    */
 	
 	// Create main label to display "Hello World!"
 	let mainLabel: UILabel
 	
 	override init(frame: CGRect) {
 		
-		// Write frame size to mainView
-		let mainViewFrame = frame
-		
-		// Create a constant for main label size
-		let mainLabelSize = CGSizeMake(240, 30)
-		
-		// Calculate main label position based on the frame size and label size.
-		let mainLabelOrigin = CGPointMake(
-			(mainViewFrame.width - mainLabelSize.width) / 2,
-			(mainViewFrame.height - mainLabelSize.height) / 2
-		)
-		
-		// Create a constant for the frame of main label
-		let mainLabelFrame = CGRectMake(mainLabelOrigin.x, mainLabelOrigin.y, mainLabelSize.width, mainLabelSize.height)
-		
 		// Create the main label
-		mainLabel = UILabel(frame: mainLabelFrame)
+		self.mainLabel = UILabel()
 		
 		// Set the text for main label
-		mainLabel.text = "Hello World!"
+		self.mainLabel.text = "Hello World!"
 		
 		// Set the text alignment for main label
-		mainLabel.textAlignment = .Center
+		self.mainLabel.textAlignment = .center
 		
 		// Initilize super class
 		super.init(frame: frame)
 		
 		// Set self background color
-		self.backgroundColor = UIColor.whiteColor()
+		self.backgroundColor = .white
 		
 		// Display main label
 		self.addSubview(mainLabel)
 	}
-
-	required init(coder aDecoder: NSCoder) {
-	    fatalError("init(coder:) has not been implemented")
+	
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
 	}
-
+	
+	override func layoutSubviews() {
+		
+		// Don't forget to call `super.layoutSubviews()`
+		super.layoutSubviews()
+		
+		// Get the desired size for main label from self size through `sizeThatFits(_:)`
+		let labelSize = self.mainLabel.sizeThatFits(self.bounds.size)
+		
+		// Calculate the origin point to put main label at the center of view
+		let x = (self.bounds.width - labelSize.width) / 2
+		let y = (self.bounds.height - labelSize.height) / 2
+		let labelOrigin = CGPoint(x: x, y: y)
+		
+		// set `frame` to main label to layout it
+		self.mainLabel.frame = CGRect(origin: labelOrigin, size: labelSize)
+		
+	}
+	
 }
