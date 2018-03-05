60e79ca9344e26fd7cbe3062158a9c573d9c8c24
星野恵瑠 <shonetesmith@gmail.com>
2018-03-06 00:08:03 +0900

    Set a title for ViewController

--- CodeBasedUI/ViewController.swift
+++ CodeBasedUI/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 		
+		// Set title
+		self.title = "A ViewController"
+		
 		// Create mainView
 		let mainView = MainView(frame: self.view.bounds)
 		
